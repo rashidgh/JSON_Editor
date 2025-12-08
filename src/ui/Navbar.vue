38b181b2e7aeb8420167a2e9d2fd027c9c234d2a
MD Rashid <lumiarashid5200@gmail.com>
2025-12-08 15:54:22 +0530

    navbar modified

--- src/ui/Navbar.vue
+++ src/ui/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
     <nav class="navbar">
-        <router-link to="/" class="links">Counter</router-link>
+        <!-- <router-link to="/" class="links">Counter</router-link>
         <router-link to="/data" class="links">Data</router-link>
         <router-link to="/form" class="links">Form</router-link>
         <router-link to="/pie" class="links">PieChart</router-link>
-        <router-link to="/levenshtein" class="links">Levenshtein</router-link>
+        <router-link to="/levenshtein" class="links">Levenshtein</router-link> -->
         <router-link to="/jsoneditor" class="links">JsonEditor</router-link>
     </nav>
 </template>
