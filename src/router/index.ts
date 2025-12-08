8f22ab3328aa87683efb55c45ae964209adafe0e
MD Rashid <lumiarashid5200@gmail.com>
2025-12-08 16:04:41 +0530

    extra code commented

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,20 @@
 // src/router/index.js
 
 import { createRouter, createWebHistory } from "vue-router";
-import Counter from "../components/Counter.vue";
-import Data from "../components/Data.vue";
-import Form from "../components/Form.vue";
-import PieChart from "../components/PieChart.vue";
-import levenshtein from "../components/Levenshtein.vue";
+// import Counter from "../components/Counter.vue";
+// import Data from "../components/Data.vue";
+// import Form from "../components/Form.vue";
+// import PieChart from "../components/PieChart.vue";
+// import levenshtein from "../components/Levenshtein.vue";
 import JsonEditor from "../components/JsonEditor.vue";
 
 const routes = [
-  { path: "/", name: "Counter", component: Counter },
-  { path: "/data", name: "Data", component: Data },
-  { path: "/form", name: "Form", component: Form },
-  { path: "/pie", name: "PieChart", component: PieChart },
-  { path: "/levenshtein", name: "Levenshtein", component: levenshtein },
-  { path: "/jsoneditor", name: "JsonEditor", component: JsonEditor },
+  { path: "/", name: "JsonEditor", component: JsonEditor },
+  // { path: "/data", name: "Data", component: Data },
+  // { path: "/form", name: "Form", component: Form },
+  // { path: "/pie", name: "PieChart", component: PieChart },
+  // { path: "/levenshtein", name: "Levenshtein", component: levenshtein },
+  // { path: "/jsoneditor", name: "JsonEditor", component: JsonEditor },
 ];
 
 const router = createRouter({
