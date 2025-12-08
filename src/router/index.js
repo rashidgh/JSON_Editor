91193679da9097a140e2d4dda89591dcf1045832
MD Rashid <lumiarashid5200@gmail.com>
2025-12-08 15:57:09 +0530

    router path change

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,12 @@ import PieChart from "../components/PieChart.vue";
 import levenshtein from "../components/Levenshtein.vue";
 import JsonEditor from "../components/JsonEditor.vue";
 const routes = [
-    { path: "/", name: "Counter", component: Counter },
-    { path: "/data", name: "Data", component: Data },
-    { path: "/form", name: "Form", component: Form },
-    { path: "/pie", name: "PieChart", component: PieChart },
-    { path: "/levenshtein", name: "Levenshtein", component: levenshtein },
-    { path: "/jsoneditor", name: "JsonEditor", component: JsonEditor },
+    { path: "/", name: "JsonEditor", component: JsonEditor },
+    // { path: "/data", name: "Data", component: Data },
+    // { path: "/form", name: "Form", component: Form },
+    // { path: "/pie", name: "PieChart", component: PieChart },
+    // { path: "/levenshtein", name: "Levenshtein", component: levenshtein },
+    // { path: "/jsoneditor", name: "JsonEditor", component: JsonEditor },
 ];
 const router = createRouter({
     history: createWebHistory(),
--- src/ui/Navbar.vue
+++ src/ui/Navbar.vue
@@ -5,7 +5,7 @@
         <router-link to="/form" class="links">Form</router-link>
         <router-link to="/pie" class="links">PieChart</router-link>
         <router-link to="/levenshtein" class="links">Levenshtein</router-link> -->
-        <router-link to="/jsoneditor" class="links">JsonEditor</router-link>
+        <router-link to="/" class="links">JsonEditor</router-link>
     </nav>
 </template>
 
